70638f61deb07a2e6878456b2bd28938f9b780ca
TerryHorizon98 <1711486313@qq.com>
2024-05-01 14:58:26 +0800

    Initialization.

---
+++ src/main/java/com/horizon/terry/stick/StickClient.java
@@ -0,0 +1,10 @@
+package com.horizon.terry.stick;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class StickClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/com/horizon/terry/stick/StickDataGenerator.java
@@ -0,0 +1,11 @@
+package com.horizon.terry.stick;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class StickDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
